22349b66773a447512e9e1bee46eb67866dda07c
Stephan Cilliers <5469870+stephancill@users.noreply.github.com>
2023-10-20 17:17:13 +0200

    fix: update minimal config/deps

--- next.config.js
+++ next.config.js
@@ -1,19 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  transpilePackages: ["@mod-protocol/react"],
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**"
-      },
-      {
-        protocol: "http",
-        hostname: "**"
-      }
-    ]
-  },
+  transpilePackages: ["@mod-protocol/react"]
 }
 
 module.exports = nextConfig
